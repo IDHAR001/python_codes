3a811f3bbe19314545db7a7450250441234afbe2
zhicong-zhang <zhicong.zhang@cnsdc01.pwc.com>
2022-10-12 12:21:03 +0800

    update

--- Fourth/Packge_demo/main.py
+++ Fourth/Packge_demo/main.py
@@ -6,3 +6,8 @@ idhar = SkillDeployer("idhar")
 idhar.SkillDeployer_print_name(idhar.name)
 SkillDeployer.SkillDeployer_print_age_static()
 idhar.SkillDeployer_print_age_static()
+
+# 导包路径
+import sys
+sys.path.append("/Users/zzhang221/Desktop/python/code/python_codes/Fourth/Packge_demo")
+print(sys.path)
